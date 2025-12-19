7c87e5bd9720d89a81aadd037194b6b593cdf524
esmrldavrgra <esmeraldavergara@u.boisestate.edu>
2025-12-19 15:49:36 -0700

    second screen added!! :)

--- Public/JavaScript/main.js
+++ Public/JavaScript/main.js
@@ -1,3 +1,14 @@
-document.querySelector(".play-btn").addEventListener("click", () => {
-  window.location.href = "/game";
+console.log("main.js loaded ✅");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const playButton = document.querySelector(".play-button");
+  const mainScreen = document.querySelector(".screen-main");
+  const messageScreen = document.querySelector(".screen-message");
+
+  if (!playButton || !mainScreen || !messageScreen) return;
+
+  playButton.addEventListener("click", () => {
+    mainScreen.classList.add("hidden");
+    messageScreen.classList.add("active");
+  });
 });
