f71f9ca795f839e56a55c8a2508a0b8fefeb8a75
esmrldavrgra <esmeraldavergara@u.boisestate.edu>
2026-01-13 20:59:15 -0700

    mini

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // tell Express where views live
-app.set("views", path.join(__dirname, "Views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // serve static files (CSS, JS, images)
-app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // home route
 app.get("/", (req, res) => {
